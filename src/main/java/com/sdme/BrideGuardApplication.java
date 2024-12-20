b9754c4de9c4c9e1d7eea171d0b0e8f525eedecd
hyunzinbak <hyunzinbak@gmail.com>
2024-12-21 00:02:00 +0900

    build 세팅

---
+++ src/main/java/com/sdme/BrideGuardApplication.java
@@ -0,0 +1,13 @@
+package com.sdme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrideGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrideGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdme/BrideGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrideGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
